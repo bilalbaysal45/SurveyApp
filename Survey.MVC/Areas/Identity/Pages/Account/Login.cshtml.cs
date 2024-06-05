b17955d341f7662c5daf3d6c670d9b3f71bdcc6f
Bilal Baysal <bilalbaysal@outlook.com>
2024-06-05 19:45:26 +0300

    User frontend
    After login Users sees this screen

--- Survey.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Survey.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -111,26 +111,26 @@ namespace Survey.MVC.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
+                //var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                if (true)//result.Succeeded
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToAction("Index", "User");
                 }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
+                // if (result.RequiresTwoFactor)
+                // {
+                //     return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                // }
+                // if (result.IsLockedOut)
+                // {
+                //     _logger.LogWarning("User account locked out.");
+                //     return RedirectToPage("./Lockout");
+                // }
+                // else
+                // {
+                //     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                //     return Page();
+                // }
             }
 
             // If we got this far, something failed, redisplay form
---
+++ Survey.MVC/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Survey.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
